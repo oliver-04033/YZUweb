1957dbb408fb389752773ad7baf4983a24d47388
oliver04033 <qunlunzheng@gmail.com>
2023-04-18 12:54:43 +0800

    update for random select

--- random select/main.js
+++ random select/main.js
@@ -1,6 +1,7 @@
 // window.onload = function(){
 //     // alert("hi")
 // };
+var pre = -1;
 let imgurl = [
     "https://attach.setn.com/newsimages/2019/01/30/1762262-XXL.jpg",
     "https://tokyo-kitchen.icook.network/uploads/recipe/cover/383547/b8c3b71ad6d3c050.jpg",
@@ -11,7 +12,16 @@ $(function(){
         // alert("hi");
         var num = $("li").length;
         var ran = Math.floor(Math.random() * num);
-        $("h1").text($("li").eq(ran).text());
-        $("img").attr("src", imgurl[ran])
+        if(ran != pre){
+            $("h1").text($("li").eq(ran).text());
+            $("img").attr("src", imgurl[ran])
+            pre = ran;
+        }
+        else{
+            ran = (ran+1)%3;
+            $("h1").text($("li").eq(ran).text());
+            $("img").attr("src", imgurl[ran])
+            pre = ran;
+        }
     })
 });
